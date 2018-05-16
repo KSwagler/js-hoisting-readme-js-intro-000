56d9b18ee034df902c501bc22d1e6c2b7f2d4580
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-16 19:48:45 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -18,11 +18,11 @@ function thisIsCrazy () {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Kristin";
+  var name = "Cricky";
+  
   function sayMy() {
+    var name = "Kristin";
     console.log(name);
-    var name = "Cricky";
-  sayMy();
-console.log (name);
   }
+  sayMy();
 }
